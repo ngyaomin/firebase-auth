c2e566d4e9b9416b796f1b2b2bca9b0342d53a23
Yao Min <ngyaomin@ymail.com>
2017-07-01 19:33:03 +0800

    setting up firebase

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
+const firebasecr = require('./firebase.json');
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      firebasecr
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
